7541ba13ec480a6a75f99975f0c4018a9b3ee0a6
victor mutai (github) <victormutaijambo@gmail.com>
2024-05-09 19:29:36 +0300

    server init

---
+++ apps/server/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+const PORT = process.env.PORT || 9999
+
+app.listen(PORT, () => {
+  console.info(`Example app listening on port ${PORT}`)
+})
