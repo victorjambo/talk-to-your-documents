daf5d7eb9b1563930cf889c60af9eaf6e8e421be
victor mutai (github) <victormutaijambo@gmail.com>
2024-05-28 21:54:24 +0300

    form validation

--- apps/docs/src/components/mainChats.tsx
+++ apps/docs/src/components/mainChats.tsx
@@ -1,5 +1,6 @@
-import React, { useRef } from "react";
+import React from "react";
 import { useMutation } from "@tanstack/react-query";
+import { useForm } from "@tanstack/react-form";
 import { ChatBubbleLeftRightIcon } from "@heroicons/react/24/outline";
 
 import Divider from "./divider";
@@ -8,8 +9,6 @@ import ChatBubbles from "./ChatBubbles";
 import { useAppData } from "../hooks/appData";
 
 const MainChats: React.FC = () => {
-  const formRef = useRef<any>();
-
   const { chatId, setConversations, conversations, chatName } = useAppData();
 
   const { mutate, isPending } = useMutation({
@@ -20,8 +19,7 @@ const MainChats: React.FC = () => {
         chatId,
       }),
     onMutate: (variables) => {
-      formRef.current?.reset();
-
+      form.reset();
       const newConversation = [
         {
           id: conversations.length + 1,
@@ -51,14 +49,31 @@ const MainChats: React.FC = () => {
       ];
       setConversations(newConversation);
     },
-    onSettled: (data, error, variables, context) => {
-      formRef.current?.reset();
+  });
+
+  const form = useForm({
+    defaultValues: {
+      prompt: "",
+    },
+    onSubmit: async ({ value: { prompt } }) => {
+      if (!chatId) {
+        console.error("Chat ID is missing");
+        return;
+      }
+      mutate(prompt);
+    },
+    validators: {
+      onSubmit: ({ value }) =>
+        value.prompt === "" ? "Ask a question first" : undefined,
     },
   });
 
-  const handleSubmit = (data: FormData) => {
-    const prompt = data.get("prompt");
-    mutate(prompt as string);
+  const formErrors = form.useStore((state) => state.errors);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    form.handleSubmit();
   };
 
   return (
@@ -69,8 +84,44 @@ const MainChats: React.FC = () => {
       <Divider classNames="py-4" />
 
       {/* <TextArea /> */}
-      <form className="relative" action={handleSubmit} ref={formRef}>
-        <textarea
+      <form className="relative" onSubmit={handleSubmit}>
+        <form.Field
+          name="prompt"
+          children={(field) => (
+            <div>
+              <textarea
+                rows={4}
+                name={field.name}
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                onChange={(e) => field.handleChange(e.target.value)}
+                placeholder={
+                  chatId
+                    ? "talk to your documents..."
+                    : "Start a conversation..."
+                }
+                className={`relative w-full p-3 rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600 ${field.state.meta.errors.length || formErrors.length ? "ring-rose-700" : ""}`}
+              />
+              {field.state.meta.errors.length ? (
+                <span className="absolute left-2 -bottom-4 text-rose-700">
+                  {field.state.meta.errors.join(", ")}
+                </span>
+              ) : null}
+              {formErrors.length ? (
+                <span className="absolute left-2 -bottom-4 text-rose-700">
+                  {formErrors.join(", ")}
+                </span>
+              ) : null}
+            </div>
+          )}
+          validators={{
+            onSubmit: ({ value }) => {
+              console.log("🚀 ~ value", value);
+              return value === "" ? "Ask a question first" : undefined;
+            },
+          }}
+        />
+        {/* <textarea
           rows={4}
           id="prompt"
           name="prompt"
@@ -78,12 +129,13 @@ const MainChats: React.FC = () => {
           placeholder={
             chatId ? "talk to your documents..." : "Start a conversation..."
           }
-        />
+          // onKeyDown={} TODO: Add keydown event to submit form
+        /> */}
         <div className="absolute right-0 bottom-0 flex justify-between m-4">
           <button
-            disabled={!isPending}
+            disabled={isPending}
             type="submit"
-            className={`inline-flex space-x-1 items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${isPending?'opacity-75 cursor-not-allowed':''}`}
+            className={`inline-flex space-x-1 items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${isPending ? "opacity-75 cursor-not-allowed" : ""}`}
           >
             <ChatBubbleLeftRightIcon className="w-5 h-5" />
             <span>Ask</span>
