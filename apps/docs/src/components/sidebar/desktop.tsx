8533731b270af2c0c6925ed45c64c603ed1d377b
victor mutai <victormutaijambo@gmail.com>
2024-09-04 01:02:31 +0300

    chore: who is Tom Cook

--- apps/docs/src/components/sidebar/desktop.tsx
+++ apps/docs/src/components/sidebar/desktop.tsx
@@ -122,20 +122,6 @@ const DesktopSidebar: React.FC<Props> = ({ chats }) => {
                 ))}
               </ul>
             </li>
-            <li className="-mx-6 mt-auto">
-              <a
-                href="#"
-                className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-50"
-              >
-                <img
-                  className="h-8 w-8 rounded-full bg-gray-50"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                />
-                <span className="sr-only">Your profile</span>
-                <span aria-hidden="true">Tom Cook</span>
-              </a>
-            </li>
           </ul>
         </nav>
       </div>
